b3ea94dc2660225c2e7d95b3ada47d9a6061393a
bang <yuudachi31@gmail.com>
2020-05-01 06:32:11 +0800

    mid ok

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MainTabNavigation from './navigations/maintab';
 import FirstScreen from './src/screens/FirstScreen';
 import SecondScreen from './src/screens/SecondScreen';
-
+import DiaryScreen from './screens/DiaryScreen'
+import breakfast from './screens/breakfast'
 const Stack = createStackNavigator();
 
 const App = () => {
@@ -15,6 +16,9 @@ const App = () => {
         <Stack.Screen options={{headerShown: false}} name="FirstScreen" component={FirstScreen} />
         <Stack.Screen options={{headerShown: false}} name="SecondScreen" component={SecondScreen} />
         <Stack.Screen options={{headerShown: false}} name="ThirdScreen" component={MainTabNavigation} />      
+      
+        <Stack.Screen options={{headerShown: false}} name="diary" component={DiaryScreen} />
+        <Stack.Screen  options={{headerShown: false}} name="breakfast" component={breakfast} />      
       </Stack.Navigator>
     </NavigationContainer>
   );
--- navigations/maintab.js
+++ navigations/maintab.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { StyleSheet, Text, View,Image } from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from '../screens/HomeScreen'
+import NewReport from '../screens/NewReport'
+import DiaryScreen from '../screens/DiaryScreen'
+import meScreen from '../screens/meScreen'
+import breakfast from '../screens/breakfast'
 import { NavigationContainer } from '@react-navigation/native';
+
 const Tab=createBottomTabNavigator();
 const MainTabNavigation =()=>{
   return (
     
+    
+
     <Tab.Navigator
     screenOptions={({ route }) => ({
       tabBarIcon: ({focused}) => {
@@ -54,9 +62,9 @@ const MainTabNavigation =()=>{
         }}
         >
             <Tab.Screen name="Home" component={HomeScreen}/>
-            <Tab.Screen name="Diary" component={HomeScreen}/>
-            <Tab.Screen name="Report" component={HomeScreen}/>
-            <Tab.Screen name="Me" component={HomeScreen}/>
+            <Tab.Screen name="Report" component={NewReport}/>
+            <Tab.Screen name="Diary" component={DiaryScreen}/>
+            <Tab.Screen name="Me" component={meScreen}/>
           </Tab.Navigator>
       )
     }
